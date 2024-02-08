528ab814bef2ae806977a9c084b7a6ffce2553c6
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-02-08 19:03:25 +0800

    :sparkles: update to 7.0.15

--- src/Migrations/AppDbContextModelSnapshot.cs
+++ src/Migrations/AppDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace atdd_v2_dotnet.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.1")
+                .HasAnnotation("ProductVersion", "7.0.15")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);

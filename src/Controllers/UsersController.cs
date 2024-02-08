b6a69a481d2dc1ca0a21314ed17f6bd1f487bb23
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-02-08 20:59:41 +0800

    :sparkles: generate token with user name

--- src/Controllers/UsersController.cs
+++ src/Controllers/UsersController.cs
@@ -1,3 +1,6 @@
+using System.Buffers.Text;
+using System.Text;
+using System.Text.Json;
 using atdd_v2_dotnet.Data;
 using atdd_v2_dotnet.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -19,7 +22,7 @@ public class UsersController : ControllerBase
     public IActionResult Post([FromBody] User user)
     {
         var loginUser = _appDbContext.Users.First(e => e.UserName == user.UserName && e.Password == user.Password);
-        Response.Headers.Add("token", "hello-world");
+        Response.Headers.Add("token", Token.MakeToken(loginUser.UserName));
         return Ok();
     }
 
@@ -55,8 +58,50 @@ public class UsersController : ControllerBase
     // }
 }
 
+    // @Data
+    // @Accessors(chain = true)
+    // public static class Token {
+    //     public static final int EXPIRATION = 300;
+    //     private String user;
+    //     private long now = Instant.now().getEpochSecond();
+    //
+    //     public static String makeToken(String userName) {
+    //         return new Token().setUser(userName).toString();
+    //     }
+    //
+    //     @SneakyThrows
+    //     public static Token parseToken(String tokenString) {
+    //         try {
+    //             return new ObjectMapper().readValue(Base64.getDecoder().decode(tokenString), Token.class);
+    //         } catch (Exception ignore) {
+    //             return null;
+    //         }
+    //     }
+    //
+    //     @JsonIgnore
+    //     public boolean isExpired() {
+    //         return Instant.now().getEpochSecond() - getNow() > EXPIRATION;
+    //     }
+    //
+    //     @Override
+    //     @SneakyThrows
+    //     public String toString() {
+    //         return Base64.getEncoder().encodeToString(new ObjectMapper().writeValueAsBytes(this));
+    //     }
+    // }
+
+
 public class Token
 {
-    public long now = DateTime.Now.Ticks;
-    public string user;
+    public long now { get; set; } = DateTime.Now.Ticks;
+    public string user { get; set; }
+
+    public static string MakeToken(string userName)
+    {
+        return new Token { user = userName }.Create();
+    }
+
+    private string Create() {
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(this)));
+    }
 }

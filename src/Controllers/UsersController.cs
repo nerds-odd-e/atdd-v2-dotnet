8890085826961006e497e92aa7f52668c1f664db
Liangchen <leeonky@gmail.com>
2024-02-08 11:06:43 +0800

    create table

--- src/Controllers/UsersController.cs
+++ src/Controllers/UsersController.cs
@@ -1,41 +1,42 @@
+using atdd_v2_dotnet.Data;
+using atdd_v2_dotnet.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace atdd_v2_dotnet.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController(AppDbContext appDbContext) : ControllerBase
     {
-        // GET: api/<API>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<User> Get()
         {
-            return new string[] { "value1", "value2" };
+            return appDbContext.Users.ToList();
         }
 
-        // GET api/<API>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<API>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<API>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<API>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        // // GET api/<API>/5
+        // [HttpGet("{id}")]
+        // public string Get(int id)
+        // {
+        //     return "value";
+        // }
+        //
+        // // POST api/<API>
+        // [HttpPost]
+        // public void Post([FromBody] string value)
+        // {
+        // }
+        //
+        // // PUT api/<API>/5
+        // [HttpPut("{id}")]
+        // public void Put(int id, [FromBody] string value)
+        // {
+        // }
+        //
+        // // DELETE api/<API>/5
+        // [HttpDelete("{id}")]
+        // public void Delete(int id)
+        // {
+        // }
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,6 @@
+using atdd_v2_dotnet.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 

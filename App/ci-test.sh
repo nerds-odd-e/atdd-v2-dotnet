e2b1ae95b34b589cbd743283bf8e5dad3e9896f0
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2024-02-21 11:03:26 +0800

    :construction_worker: extract coverage number

--- App/ci-test.sh
+++ App/ci-test.sh
@@ -2,4 +2,5 @@
 docker-compose exec jdk ./gradlew cucumber
 docker-compose exec dotnet bash -c "kill $(docker-compose exec dotnet pidof dotnet)"
 ./until_log.sh dotnet 'e2e-coverage-done'
-cat coverage.cobertura.xml
+#cat coverage.cobertura.xml
+docker compose logs dotnet
